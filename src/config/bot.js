a9e9a71a38a5887c3eb328f1e0fe414e6c26b71c
twelfth <145135548+Prestorose@users.noreply.github.com>
2026-05-26 04:29:53 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "caca",
+        name: "🔴 A Blood Moon is approaching...",
         // Activity type number (0 = Playing).
         type: 0, 
       },
